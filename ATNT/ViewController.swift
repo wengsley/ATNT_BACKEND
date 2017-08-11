a672d8f32f5d086150e970febf7752d2b6311d68
Sam Pin Sang <sampinsang@Sams-MacBook-Pro.local>
2017-08-11 18:10:41 +0800

    Initial Commit

---
+++ ATNT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATNT
+//
+//  Created by Sam Pin Sang on 11/08/2017.
+//  Copyright © 2017 samgdx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
