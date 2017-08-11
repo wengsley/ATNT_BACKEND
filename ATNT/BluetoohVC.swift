e851b630437b1d9d963e93a334926c02e64258fa
Sam Pin Sang <sampinsang@Sams-MacBook-Pro.local>
2017-08-11 21:38:31 +0800

    first commit

---
+++ ATNT/BluetoohVC.swift
@@ -0,0 +1,35 @@
+//
+//  BluetoohVC.swift
+//  ATNT
+//
+//  Created by Sam Pin Sang on 11/08/2017.
+//  Copyright © 2017 samgdx. All rights reserved.
+//
+
+import UIKit
+
+class BluetoohVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
